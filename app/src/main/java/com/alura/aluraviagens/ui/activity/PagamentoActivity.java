a45a117b8b71436e1b7e3dff1bcad8077bc4fe7b
FelipeIgansi <lipeigansi@gmail.com>
2022-10-12 21:41:34 -0300

    Refatoracao

--- app/src/main/java/com/alura/aluraviagens/ui/activity/PagamentoActivity.java
+++ app/src/main/java/com/alura/aluraviagens/ui/activity/PagamentoActivity.java
@@ -3,14 +3,34 @@ package com.alura.aluraviagens.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.alura.aluraviagens.R;
+import com.alura.aluraviagens.model.Pacote;
+import com.alura.aluraviagens.util.MoedaUtil;
+
+import java.math.BigDecimal;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
+
+        setTitle(TITULO_APPBAR);
+
+        Pacote pacoteSP = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal(243.99));
+
+        mostraPreco(pacoteSP);
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.pagamento_TxtPreco);
+        String moedaReal = MoedaUtil.formataPara_MoedaReal(pacote.getPreco());
+        preco.setText(moedaReal);
     }
 }
